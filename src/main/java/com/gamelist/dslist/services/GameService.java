d4283b0acf9b8575614bd9746e7ba1befc1d935b
tcedro <thiago.cedro@sga.pucminas.brgit config --global user.name tcedrogit config --global user.email thiago.cedro@sga.pucminas.br>
2023-07-10 23:00:36 -0300

    dto, service, controller, repository

---
+++ src/main/java/com/gamelist/dslist/services/GameService.java
@@ -0,0 +1,20 @@
+package com.gamelist.dslist.services;
+
+import com.gamelist.dslist.dto.GameMinDTO;
+import com.gamelist.dslist.entities.Game;
+import com.gamelist.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+}
