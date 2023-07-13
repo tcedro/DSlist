01ac8338653af12749b648b8204fb8c4852160da
tcedro <thiago.cedro@sga.pucminas.brgit config --global user.name tcedrogit config --global user.email thiago.cedro@sga.pucminas.br>
2023-07-13 15:14:48 -0300

    Homolog ok

--- src/main/java/com/gamelist/dslist/dto/GameMinDTO.java
+++ src/main/java/com/gamelist/dslist/dto/GameMinDTO.java
@@ -25,7 +25,7 @@ public class GameMinDTO {
     public GameMinDTO(GameMinProjection entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
-        this.year = entity.getYear();
+        this.year = entity.getGameYear();
         this.imgUrl = entity.getImgUrl();
         this.shortDescription = entity.getShortDescription();
     }
